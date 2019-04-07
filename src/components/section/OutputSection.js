14129aeb546f481548d2eff7e56535a74bb5cbcf
Chike <hyginuschinwoke@gmail.com>
2019-04-07 18:51:21 +0100

    ft(export): export generated phone numbers to file - display minimum, maximum and total number of generated numbers - create method for exporting/saving generated phone numbers to file on local machine

--- src/components/section/OutputSection.js
+++ src/components/section/OutputSection.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import styled from "styled-components";
 import { array } from "prop-types";
+import saveAs from "file-saver";
 
 const TextArea = styled.div`
   outline: none;
@@ -33,8 +34,28 @@ const Button = styled.button`
     color: white;
     background-color: #de3e5f;
   }
-  margin: 10px 0 0 0;
+  margin: 14px 0 0 0;
   outline: none;
+`;
+const TableArea = styled.div`
+  width: 100%;
+  margin-top: 20px;
+  font-size: 20px;
+  border: 2px solid #555;
+  box-shadow: 5px 5px 5px #aaaaaa;
+  table {
+    width: inherit;
+    text-align: left;
+    border-collapse: collapse;
+    thead {
+      background-color: #555;
+      color: white;
+    }
+    td,
+    th {
+      padding: 10px;
+    }
+  }
 `;
 
 const displayNumbers = phoneNumbers => {
@@ -53,11 +74,54 @@ const displayNumbers = phoneNumbers => {
   ));
 };
 
+const calculateMinMax = phoneNumbers => {
+  const min = Math.min(...phoneNumbers);
+  const max = Math.max(...phoneNumbers);
+  const total = phoneNumbers.length;
+
+  return { min, max, total };
+};
+
+const displayTable = phoneNumbers => {
+  const { min, max, total } = calculateMinMax(phoneNumbers);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Min Number</th>
+          <th>Max Number</th>
+          <th>Total Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>0{min}</td>
+          <td>0{max}</td>
+          <td>{total}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const savePhoneNumbers = () => {
+  const numbers = document.getElementById("numbers").innerText;
+  const file = new File([numbers], {
+    type: "text/plain;charset=utf-8"
+  });
+  saveAs(file, "phone-numbers.txt");
+};
+
 const OutPutSection = ({ phoneNumbers }) => {
   return (
     <Fragment>
-      <TextArea readOnly>{displayNumbers(phoneNumbers)}</TextArea>
-      <Button>Export</Button>
+      <TextArea readOnly id="numbers">{displayNumbers(phoneNumbers)}</TextArea>
+      {phoneNumbers.length > 0 && (
+        <Fragment>
+          <Button onClick={() => savePhoneNumbers(phoneNumbers)}>Export</Button>
+          <TableArea>{displayTable(phoneNumbers)}</TableArea>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
